7fc94d29c3a69478593cef9939b336e518665e88
sahilGidwani-26 <gidwanisahil80@gmail.com>
2025-07-25 13:48:35 +0530

    Delete src directory

--- src/components/Navbar.js
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="bg-black text-white px-6 py-4 shadow-md">
-      <h1 className="text-xl font-semibold">🔒 SecureSight</h1>
-    </nav>
-  );
-}
